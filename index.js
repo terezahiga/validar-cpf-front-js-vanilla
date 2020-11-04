a07c87ce295aec952dcd79606416dc31a34c1410
tereza_higa <tereza.higa@gmail.com>
2020-11-04 10:17:40 -0300

    msg1

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const URL="https://java-api-spring-bot.herokuapp.com/validacpf?cpf="
+//const URL="https://java-api-spring-bot.herokuapp.com/validacpf?cpf="
+const URL="https://vanilla-cpf-tt.herokuapp.com/validacpf?cpf="
 function validar(){
 
 	var txtCPF = document.getElementById("txtCpf").value;
